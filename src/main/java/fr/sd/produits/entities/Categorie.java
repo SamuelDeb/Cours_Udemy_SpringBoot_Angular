bcc860a60e525d75eba2392d78f3e2cb1f7b3715
samuel <samuel.debaer@gmail.com>
2025-08-19 20:14:23 +0200

    initialisation du projet cours spring_angular

---
+++ src/main/java/fr/sd/produits/entities/Categorie.java
@@ -0,0 +1,34 @@
+package fr.sd.produits.entities;
+
+import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+public class Categorie {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idCat;
+	private String nomCat;
+	private String descriptionCat;
+	
+	@OneToMany(mappedBy = "categorie")
+	private List<Produit> produits;
+
+	public void setIdCat(long l) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
