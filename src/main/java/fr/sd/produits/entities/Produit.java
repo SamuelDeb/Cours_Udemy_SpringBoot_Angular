30add50d11f34a3f39a027ffec5518950891eea1
SamuelDeb <92782774+SamuelDeb@users.noreply.github.com>
2025-08-22 15:45:19 +0200

    Update Produit.java
    Ajout javadoc

--- src/main/java/fr/sd/produits/entities/Produit.java
+++ src/main/java/fr/sd/produits/entities/Produit.java
@@ -1,82 +1,167 @@
 package fr.sd.produits.entities;
+
 import java.util.Date;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+
+/**
+ * Représente un produit dans le système.
+ * Cette classe est une entité JPA et est mappée à une table en base de données.
+ */
 @Entity
 public class Produit {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long idProduit;
-	private String nomProduit;
-	private Double prixProduit;
-	private Date dateCreation;
-	
-	@ManyToOne
-	private Categorie categorie;
 
-	
-	
+    /**
+     * Identifiant unique du produit.
+     * Généré automatiquement par la base de données.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProduit;
 
-	public Produit() {
-		super();
-	}
+    /**
+     * Nom du produit.
+     */
+    private String nomProduit;
 
-	public Produit(String nomProduit, Double prixProduit, Date dateCreation) {
-		super();
-		this.nomProduit = nomProduit;
-		this.prixProduit = prixProduit;
-		this.dateCreation = dateCreation;
-	}
+    /**
+     * Prix du produit.
+     */
+    private Double prixProduit;
 
+    /**
+     * Date de création du produit.
+     */
+    private Date dateCreation;
 
+    /**
+     * Catégorie à laquelle appartient le produit.
+     */
+    @ManyToOne
+    private Categorie categorie;
 
-	public Long getIdProduit() {
-		return idProduit;
-	}
+    /**
+     * Constructeur par défaut.
+     */
+    public Produit() {
+        super();
+    }
 
-	public void setIdProduit(Long idProduit) {
-		this.idProduit = idProduit;
-	}
+    /**
+     * Constructeur avec paramètres.
+     *
+     * @param nomProduit   Le nom du produit.
+     * @param prixProduit  Le prix du produit.
+     * @param dateCreation La date de création du produit.
+     */
+    public Produit(String nomProduit, Double prixProduit, Date dateCreation) {
+        super();
+        this.nomProduit = nomProduit;
+        this.prixProduit = prixProduit;
+        this.dateCreation = dateCreation;
+    }
 
-	public String getNomProduit() {
-		return nomProduit;
-	}
+    /**
+     * Retourne l'identifiant du produit.
+     *
+     * @return L'identifiant du produit.
+     */
+    public Long getIdProduit() {
+        return idProduit;
+    }
 
-	public void setNomProduit(String nomProduit) {
-		this.nomProduit = nomProduit;
-	}
+    /**
+     * Définit l'identifiant du produit.
+     *
+     * @param idProduit L'identifiant du produit à définir.
+     */
+    public void setIdProduit(Long idProduit) {
+        this.idProduit = idProduit;
+    }
 
-	public Double getPrixProduit() {
-		return prixProduit;
-	}
+    /**
+     * Retourne le nom du produit.
+     *
+     * @return Le nom du produit.
+     */
+    public String getNomProduit() {
+        return nomProduit;
+    }
 
-	public void setPrixProduit(Double prixProduit) {
-		this.prixProduit = prixProduit;
-	}
+    /**
+     * Définit le nom du produit.
+     *
+     * @param nomProduit Le nom du produit à définir.
+     */
+    public void setNomProduit(String nomProduit) {
+        this.nomProduit = nomProduit;
+    }
 
-	public Date getDateCreation() {
-		return dateCreation;
-	}
+    /**
+     * Retourne le prix du produit.
+     *
+     * @return Le prix du produit.
+     */
+    public Double getPrixProduit() {
+        return prixProduit;
+    }
 
-	public void setDateCreation(Date dateCreation) {
-		this.dateCreation = dateCreation;
-	}
-	
-	public Categorie getCategorie() {
-		return categorie;
-	}
+    /**
+     * Définit le prix du produit.
+     *
+     * @param prixProduit Le prix du produit à définir.
+     */
+    public void setPrixProduit(Double prixProduit) {
+        this.prixProduit = prixProduit;
+    }
 
-	public void setCategorie(Categorie categorie) {
-		this.categorie = categorie;
-	}
+    /**
+     * Retourne la date de création du produit.
+     *
+     * @return La date de création du produit.
+     */
+    public Date getDateCreation() {
+        return dateCreation;
+    }
 
-	@Override
-	public String toString() {
-		return "Produit [idProduit=" + idProduit + ", nomProduit=" + nomProduit + ", prixProduit=" + prixProduit
-				+ ", dateCreation=" + dateCreation + "]";
-	}
+    /**
+     * Définit la date de création du produit.
+     *
+     * @param dateCreation La date de création du produit à définir.
+     */
+    public void setDateCreation(Date dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    /**
+     * Retourne la catégorie du produit.
+     *
+     * @return La catégorie du produit.
+     */
+    public Categorie getCategorie() {
+        return categorie;
+    }
+
+    /**
+     * Définit la catégorie du produit.
+     *
+     * @param categorie La catégorie du produit à définir.
+     */
+    public void setCategorie(Categorie categorie) {
+        this.categorie = categorie;
+    }
+
+    /**
+     * Retourne une représentation textuelle du produit.
+     *
+     * @return Une chaîne de caractères représentant le produit.
+     */
+    @Override
+    public String toString() {
+        return "Produit [idProduit=" + idProduit + ", nomProduit=" + nomProduit + ", prixProduit=" + prixProduit
+                + ", dateCreation=" + dateCreation + "]";
+    }
 }
